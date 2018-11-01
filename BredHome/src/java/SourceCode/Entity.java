665976e43b097d68741e6df4f77195621ec3c1a9
Иван <@me>
2018-11-02 01:51:38 +0300

    Осуществлено клонирование на домашний ноут

---
+++ BredHome/src/java/SourceCode/Entity.java
@@ -0,0 +1,24 @@
+
+package SourceCode;
+import java.io.Serializable;
+
+public abstract class Entity implements Serializable, Cloneable{
+    
+    int id;
+    
+    public Entity() {
+        
+    }
+    
+    public Entity(int id) {
+        this.id = id;
+    }
+    
+    public int getId() {
+        return id;
+    }
+    
+    public void setId(int id) {
+        this.id = id;
+    }
+}
